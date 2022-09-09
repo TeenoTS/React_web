a446296790be29f14d3f213693c965b26451318f
Teeno <tino_08478@hotmail.com>
2022-09-09 10:14:18 +0700

    first commit

---
+++ app1/src/App.js
@@ -0,0 +1,14 @@
+import Eventdata2 from './event-data';
+import './App.css';
+import Banner from './banner';
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+      <br/>
+      <Eventdata2/>
+    </div>
+  );
+}
+
+export default App;
